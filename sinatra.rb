52bd742f590c4ee4a7fdf8dc5cd562b128dfcd38
Alexey Mazurik <alexeymazurik@gmail.com>
2014-10-23 01:34:55 +0300

    first commit

---
+++ sinatra.rb
@@ -0,0 +1,18 @@
+require 'sinatra'
+
+get '/' do
+  erb :home
+end
+
+get '/memory' do
+  ram = `top -l 1 | head -n 10 | grep PhysMem | sed 's/, /n /g'`
+	erb :memory, :locals => { :ramInfo => ram }
+end
+
+
+get '/disk' do
+   diskArray = `df -h`.lines
+   erb :disk, :locals => { :all => diskArray }
+end
+
+
